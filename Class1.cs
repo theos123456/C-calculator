1376b9b81d2132fa78df185f0fc3c2d17bb6191e
theos123456 <41969927+theos123456@users.noreply.github.com>
2018-12-13 21:43:12 +0200

    FIRST COMMIT

---
+++ Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Menu
+{
+    class Class1
+    {
+        
+        
+      static public double add (Double a, Double b )
+        {
+         
+
+            return a + b;
+        }
+        static public double sub(Double a, Double b )
+        {
+            return a - b;
+        }
+        static public double div (Double a, Double b )
+        {
+              return a / b; 
+           
+            
+        }
+        static public double mul(Double a, Double b )
+        {
+            return a * b;
+        }
+
+
+    }
+}
